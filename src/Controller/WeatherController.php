32392d2a5b03d98395964456989da67f4f3bbde8
Piotr Wróbel <wb.ptr01@gmail.com>
2024-10-22 18:12:57 +0200

    Finished lab-c

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MeasurementRepository;
+use App\Repository\LocationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{city}/{countryCode?}', name: 'app_weather', requirements: ['city' => '[a-zA-Z]+', 'countryCode' => '[A-Z]{2}'])]
+    public function city(string $city, ?string $countryCode, LocationRepository $locationRepository, MeasurementRepository $measurementRepository): Response
+    {
+        $location = $locationRepository->myFuncFindBy($city, $countryCode);
+        $measurements = $measurementRepository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+}
+
+
+
+//<?php
+
+//namespace App\Controller;
+
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Attribute\Route;
+//
+//class WeatherController extends AbstractController
+//{
+//    #[Route('/weather', name: 'app_weather')]
+//    public function index(): Response
+//    {
+//        return $this->render('weather/index.html.twig', [
+//            'controller_name' => 'WeatherController',
+//        ]);
+//    }
+//}
---
+++ src/Repository/LocationRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Location;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Location>
+ */
+class LocationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Location::class);
+    }
+
+    public function myFuncFindBy(string $city, ?string $countryCode): ?Location
+    {
+        $qb = $this->createQueryBuilder('l')
+            ->andWhere('l.city = :city')
+            ->setParameter('city', $city);
+
+        if ($countryCode) {
+            $qb->andWhere('l.country = :country')
+                ->setParameter('country', $countryCode);
+        }
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+    //    /**
+    //     * @return Location[] Returns an array of Location objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('l.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Location
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
